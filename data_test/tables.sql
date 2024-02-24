99580670b1d9d513bdbd79469023cabd04f76e4f
Rodolphe Quiedeville <r.quiedeville@smartway.ai>
2024-02-24 12:18:39 +0100

    Add data tests

---
+++ data_test/tables.sql
@@ -0,0 +1,37 @@
+DROP TABLE IF EXISTS race;
+DROP TABLE IF EXISTS results;
+DROP TABLE IF EXISTS boat;
+DROP TABLE IF EXISTS skipper;
+
+CREATE TABLE boat (
+  id serial primary key,
+  name text,
+  created_at timestamp with time zone default now(),
+  updated_at timestamp without time zone,
+  length integer,
+  class_code varchar(10)
+);
+
+
+CREATE TABLE skipper (
+  id serial primary key,
+  name text,
+  age int,
+  created_at timestamp with time zone default now(),
+  updated_at timestamp without time zone,
+  price float default 4.5
+);
+
+CREATE TABLE race (
+  id serial primary key,
+  name text,
+  year int
+);
+
+/* This table is never empty on destination
+ *
+ */
+CREATE TABLE results (
+  id serial primary key,
+  year int
+);
