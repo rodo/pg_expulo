72e21f1aea3500edf1d356197b96ad19126aa73f
Rodolphe Quiedeville <r.quiedeville@smartway.ai>
2024-02-26 16:07:50 +0100

    Align test on data test

--- data_test/skipper_test.sql
+++ data_test/skipper_test.sql
@@ -8,8 +8,8 @@ SELECT has_table('public', 'skipper', 'table skipper exists');
 -- all the wanted rows are here and no more
 SELECT results_eq(
     'SELECT count(*)::int FROM skipper',
-    'SELECT 1::int',
-    'There is 3 rows in table skipper');
+    'SELECT 10::int',
+    'There is 10 rows in table skipper');
 
 -- the column name is masked
 SELECT row_eq(
