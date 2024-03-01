af0767f9b85e0478f84567e1c3bc6c72f1a8c8aa
Rodolphe Quiedeville <r.quiedeville@smartway.ai>
2024-03-01 12:12:22 +0100

    Add some data

--- data_test/serial/data.sql
+++ data_test/serial/data.sql
@@ -7,11 +7,18 @@ BEGIN;
 
 SET search_path = 'laires';
 
-INSERT INTO root (name) SELECT generate_series(1, 40, 1)::text;
+INSERT INTO root (name) SELECT generate_series(1, 4000, 1)::text;
 
-INSERT INTO la (topid,name) SELECT generate_series(1, 10, 1), generate_series(1, 10, 1);
+INSERT INTO la (topid,name) SELECT generate_series(1, 100, 1), generate_series(1, 100, 1);
 
-INSERT INTO lb (topid,name) SELECT generate_series(2, 10, 1), generate_series(1, 10, 1);
+INSERT INTO lb (topid,name) SELECT generate_series(2, 100, 2), generate_series(1, 100, 1);
+
+
+INSERT INTO lbb (lbid,name) SELECT id, name FROM lb ORDER BY RANDOM() LIMIT 10;
+INSERT INTO lbb (lbid,name) SELECT id, name FROM lb ORDER BY RANDOM() LIMIT 100;
+
+INSERT INTO laa (laid,name) SELECT id, name FROM la ORDER BY RANDOM() LIMIT 10;
+INSERT INTO laa (laid,name) SELECT id, name FROM la ORDER BY RANDOM() LIMIT 100;
 
 
 COMMIT ;
