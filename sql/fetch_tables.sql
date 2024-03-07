4d64dacbf1a6f50a84768fdeea4059cb421f0f42
Rodolphe Quiedeville <r.quiedeville@smartway.ai>
2024-03-07 09:45:49 +0100

    Add option to generate the config file

---
+++ sql/fetch_tables.sql
@@ -0,0 +1,10 @@
+SELECT
+        n.nspname as schema,
+        c.relname as tablename
+
+FROM pg_class c
+INNER JOIN pg_namespace n ON n.oid = c.relnamespace
+WHERE
+        c.relkind = 'r'
+AND
+        n.nspname NOT in ('pg_catalog', 'pg_toast', 'information_schema')
